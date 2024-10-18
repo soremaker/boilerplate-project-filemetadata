a9e03482902ef3754b7c7fbc8872ef41b205c211
soremaker <95688706+soremaker@users.noreply.github.com>
2024-10-18 10:14:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+const multer = require('multer');
+const upload = multer({dest: 'api/fileanalyse'});
+app.post('/api/fileanalyse',upload.single('upfile') ,function (req, res) {
+  const file = req.file;
+  //console.log(file);
+  if(file){
+    res.json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size
+    });
+  }else{
+    res.json({
+      error: 'No file uploaded'
+    });
+  }
+})
 
 
 const port = process.env.PORT || 3000;
